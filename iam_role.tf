dcc2558d1868a5b951d6e7552a7aca4f89be8761
Rahul meena <rahul159meena@gmail.com>
2021-03-05 17:24:16 +0530

    iam role

---
+++ iam_role.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_role" "ecs-instance-role" {
+    name = "test-role"
+    path = "/"  
+
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment" {
+  role       = aws_iam_role.ecs-instance-role.name
+  policy_arn = "arn:aws:iam::aws:policy/aws-service-role/AmazonECSServiceRolePolicy"
+}
